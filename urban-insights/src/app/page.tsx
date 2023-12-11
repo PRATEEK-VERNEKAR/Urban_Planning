9e8c33d25060855ac89f79b60a4c68b42820f83b
satish kolde <satishkolde335@gmail.com>
2023-12-11 05:57:29 -0600

    check

--- urban-insights/src/app/page.tsx
+++ urban-insights/src/app/page.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
 
-      <div >
+      <div className='bg-transparent' >
         <a 
         href='/ui/dashboard'
-        className='w-36 h-36 flex justify-center items-center rounded-full bg-orange-800'>
-          Explore Earth
+        className='sh-btn overpass txt-16 font-bold text-[#D8D8D8] w-24 h-24 flex justify-center items-center rounded-full bg-[#44C1E2] bg-opacity-75 animate__animated animate__zoomIn'>
+          Explore
         </a>        
       </div>
   )
